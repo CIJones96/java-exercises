2123f0255f689725a05e71422deb5ec49dbbb630
Chris Jones <chrisj96@proton.me>
2026-04-04 23:28:57 +0100

    Complete Person and Unit Tests

--- src/basics/Person.java
+++ src/basics/Person.java
@@ -1,4 +1,29 @@
 package basics;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private String occupation;
+
+    public Person(String name, int age, String occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getOccupation() { return occupation; }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name +
+                ", I'm " + age +
+                " and I work as a " + occupation);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Chris", 28, "Java Developer");
+        person.introduce();
+    }
 }
--- test/basics/PersonTest.java
+++ test/basics/PersonTest.java
@@ -1,2 +1,28 @@
-public class PersonTest {
+import basics.Person;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    Person person = new Person("Chris", 28, "Java Developer");
+
+    @Test
+    void testGetName() {
+        assertEquals("Chris", person.getName());
+    }
+
+    @Test
+    void testGetAge() {
+        assertEquals(28, person.getAge());
+    }
+
+    @Test
+    void testGetOccupation() {
+        assertEquals("Java Developer", person.getOccupation());
+    }
+
+    @Test
+    void testIntroduce() {
+        person.introduce();
+    }
 }
