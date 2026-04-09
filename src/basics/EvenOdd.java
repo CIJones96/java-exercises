a91a09c5e2b150278d83cf5d90cb4830f7e0d7c1
Chris Jones <chrisj96@proton.me>
2026-04-09 19:25:30 +0100

    Adding code for EvenOdd.java

--- src/basics/EvenOdd.java
+++ src/basics/EvenOdd.java
@@ -1,4 +1,15 @@
 package basics;
 
 public class EvenOdd {
+
+    public static String check(int n) {
+        if (n % 2 == 0) return "Even";
+        return "Odd";
+    }
+
+    public static void main(String[] args) {
+        System.out.print(check(4));
+        System.out.print(check(7));
+        System.out.print(check(0));
+    }
 }
