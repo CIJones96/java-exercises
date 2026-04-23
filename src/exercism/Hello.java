3f293bad55a364b10c677b1b1ecc4214667abb89
Chris Jones <chrisj96@proton.me>
2026-04-23 22:31:04 +0100

    Create Hello.java - Linking with main method

---
+++ src/exercism/Hello.java
@@ -0,0 +1,12 @@
+package exercism;
+
+public class Hello {
+
+    static String getGreeting() {
+        return "Hello, World";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+}
