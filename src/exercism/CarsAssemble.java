9070b2ca17ad2be33bed611f01100d223ecc67ce
Chris Jones <chrisj96@proton.me>
2026-05-11 19:33:55 +0100

    Exercism Exercise - Add CarAssemble.class

---
+++ src/exercism/CarsAssemble.java
@@ -0,0 +1,12 @@
+package exercism;
+
+public class CarsAssemble {
+
+    public double productionRatePerHour(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    }
+}
